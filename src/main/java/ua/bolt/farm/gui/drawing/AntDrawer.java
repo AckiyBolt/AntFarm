01d3097ec5299f5f824ab0c4a0e3b4327fd028c7
AckiyBolt <ackiybolt@gmail.com>
2014-12-08 19:41:34 +0200

    color randomization upgrade

--- src/main/java/ua/bolt/farm/gui/drawing/AntDrawer.java
+++ src/main/java/ua/bolt/farm/gui/drawing/AntDrawer.java
@@ -6,49 +6,25 @@ import ua.bolt.farm.ant.MovementLogger;
 import ua.bolt.farm.field.Field;
 import ua.bolt.farm.field.entity.Coordinates;
 
-import java.util.*;
+import java.util.Iterator;
 
 /**
  * Created by ackiybolt on 08.12.14.
  */
 public class AntDrawer extends AbstractDrawer {
 
-    private List<Color> colors;
-    private HashSet<Color> usedColors;
-
-    private Random rnd = new Random(System.nanoTime());
+    private ColorRandomizer colorRandomizer;
 
     public AntDrawer() {
-        // Create the magic cache of rainbow...
-        java.lang.reflect.Field[] fields = Color.class.getFields();
-        ArrayList<Color> colors = new ArrayList<Color>(fields.length);
-
-        for (java.lang.reflect.Field field : fields) {
-            try {
-                Object obj = field.get(Color.class);
-                if (obj instanceof Color) {
-                    Color color = (Color) obj;
-                    if (color.isOpaque())
-                        colors.add(color);
-                }
-
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-
-        this.colors = Collections.<Color>unmodifiableList(colors);
-        this.usedColors = new HashSet<Color>(colors.size());
+        colorRandomizer = new ColorRandomizer();
     }
 
     public void draw(Field field, MovementLogger... movementLoggers) {
 
-        usedColors.clear();
-
         for (int i = 0; i < movementLoggers.length; i++) {
             MovementLogger logger = movementLoggers[i];
 
-            Color color = getRandomColor();
+            Color color = colorRandomizer.getRandomColor();
 
             context.setStroke(color);
             context.setFill(color);
@@ -66,19 +42,6 @@ public class AntDrawer extends AbstractDrawer {
                 number + " - " + ownerName,
                 10,
                 number * fontSize + fontSize);
-    }
-
-    private Color getRandomColor() {
-
-        if (usedColors.size() > colors.size() / 1.4)
-            usedColors.clear();
-
-        Color chosenColor = colors.get(rnd.nextInt(colors.size()));
-        if (usedColors.contains(chosenColor))
-            return getRandomColor();
-        else
-            usedColors.add(chosenColor);
-        return chosenColor;
     }
 
     private void drawPath(GraphicsContext context, MovementLogger logger) {
---
+++ src/main/java/ua/bolt/farm/gui/drawing/ColorRandomizer.java
@@ -0,0 +1,81 @@
+package ua.bolt.farm.gui.drawing;
+
+import javafx.scene.paint.Color;
+
+import java.util.*;
+
+/**
+ * Created by ackiybolt on 08.12.14.
+ */
+public class ColorRandomizer {
+
+    private static final double COLOR_LIMIT = 0.3;
+    private static final double GRADATION_STEP = 0.2;
+
+    private Queue<Color> colors;
+
+    public ColorRandomizer () {
+        LinkedList<Color> colors = new LinkedList<>();
+
+        generateOneChColors(colors);
+        generateTwoChColors(colors);
+
+        Collections.shuffle(colors, new Random(System.nanoTime()));
+
+        System.out.println(colors.size());
+        this.colors = colors;
+    }
+
+    public Color getRandomColor() {
+
+        Color chosenColor = colors.poll();
+        colors.add(chosenColor);
+
+        return chosenColor;
+    }
+
+    private void generateOneChColors(List<Color> colors) {
+
+        for (Chanel ch: Chanel.values()) {
+            switch (ch) {
+                case RED:
+                    for (double i = 1.0; i >= COLOR_LIMIT; i = i - GRADATION_STEP)
+                        colors.add(Color.color(i, 0.0, 0.0));
+                    break;
+                case GREEN:
+                    for (double i = 1.0; i >= COLOR_LIMIT; i = i - GRADATION_STEP)
+                        colors.add(Color.color(0.0, i, 0.0));
+                    break;
+                case BLUE:
+                    for (double i = 1.0; i >= COLOR_LIMIT; i = i - GRADATION_STEP)
+                        colors.add(Color.color(0.0, 0.0, i));
+                    break;
+            }
+        }
+    }
+
+    private void generateTwoChColors(List<Color> colors) {
+        for (Chanel ch: Chanel.values()) {
+            switch (ch) {
+                case RED:
+                    for (double i = 1.0; i >= COLOR_LIMIT; i = i - GRADATION_STEP)
+                        colors.add(Color.color(i, i, 0.0));
+                    break;
+                case GREEN:
+                    for (double i = 1.0; i >= COLOR_LIMIT; i = i - GRADATION_STEP)
+                        colors.add(Color.color(0.0, i, i));
+                    break;
+                case BLUE:
+                    for (double i = 1.0; i >= COLOR_LIMIT; i = i - GRADATION_STEP)
+                        colors.add(Color.color(i, 0.0, i));
+                    break;
+            }
+        }
+    }
+
+    private enum Chanel {
+        RED,
+        GREEN,
+        BLUE;
+    }
+}
